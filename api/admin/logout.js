bdcbc78f55386683bdff1b589ea553acd00a33a5
sggbwbs <125610219+sggbwbs@users.noreply.github.com>
2026-07-07 22:29:26 +0300

    Delete api/admin/logout.js

--- api/admin/logout.js
+++
@@ -1,7 +0,0 @@
-const { clearSessionCookie } = require('./_auth');
-
-module.exports = async (req, res) => {
-  if (req.method !== 'POST') return res.status(405).end();
-  clearSessionCookie(res);
-  res.status(200).json({ ok: true });
-};
